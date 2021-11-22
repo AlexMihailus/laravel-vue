d7e87dc63a02d1100b8f79fbc7ceafcf6d0a8ded
Alex Mihailus <mihailus.A.G@gmail.com>
2021-11-22 12:15:32 +0200

    initial commit

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <SinglePostComponent />
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Age</th>
+          <th scope="col">Job</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="person in persons" :key="person.id">
+          <th scope="row">{{ person.id }}</th>
+          <td>{{ person.name }}</td>
+          <td>{{ person.age }}</td>
+          <td>{{ person.job }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import SinglePostComponent from "./SinglePostComponent";
+export default {
+  name: "PostComponent",
+
+  data() {
+    return {
+      persons: null,
+    };
+  },
+
+  mounted() {
+      this.getPersons()
+  },
+
+  methods: {
+    getPersons() {
+      axios.get("/persons")
+      .then((res) => {
+          this.persons = res.data
+      })
+      .catch( error => {
+
+      })
+      .finally({
+          
+      })
+    }
+  },
+
+  components: {
+    SinglePostComponent,
+  },
+};
+</script>
+
+<style scoped>
+</style>
