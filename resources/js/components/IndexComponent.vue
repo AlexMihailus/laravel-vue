54ef98509f365480d9e9ff860fecfddb8f35bda8
Alex Mihailus <mihailus.A.G@gmail.com>
2021-11-22 21:41:50 +0200

    add updating the person

--- resources/js/components/IndexComponent.vue
+++ resources/js/components/IndexComponent.vue
@@ -7,15 +7,47 @@
           <th scope="col">Name</th>
           <th scope="col">Age</th>
           <th scope="col">Job</th>
+          <th scope="col">Edit</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="person in people" :key="person.id">
-          <th scope="row">{{ person.id }}</th>
-          <td>{{ person.name }}</td>
-          <td>{{ person.age }}</td>
-          <td>{{ person.job }}</td>
-        </tr>
+        <template v-for="person in people">
+          <tr :key="person.id" :class="isEdit(person.id) ? 'd-none' : ''">
+            <th scope="row">{{ person.id }}</th>
+            <td>{{ person.name }}</td>
+            <td>{{ person.age }}</td>
+            <td>{{ person.job }}</td>
+            <td>
+              <a
+                href="#"
+                @click.prevent="
+                  changeEditPersonId(
+                    person.id,
+                    person.name,
+                    person.age,
+                    person.job
+                  )
+                "
+                class="btn btn-success"
+                >Edit</a
+              >
+            </td>
+          </tr>
+          <tr :class="isEdit(person.id) ? '' : 'd-none'">
+            <th scope="row">{{ person.id }}</th>
+            <td><input type="text" v-model="name" class="form-control" /></td>
+            <td><input type="text" v-model="age" class="form-control" /></td>
+            <td><input type="text" v-model="job" class="form-control" /></td>
+            <td>
+              <a
+                href="#"
+                @click.prevent="updatePerson(person.id)"
+                class="btn btn-success"
+                >Update</a
+              >
+            </td>
+          </tr>
+        </template>
       </tbody>
     </table>
   </div>
@@ -27,7 +59,11 @@ export default {
 
   data() {
     return {
-        people: null
+      people: null,
+      edidPersonId: null,
+      name: null,
+      age: null,
+      job: null,
     };
   },
 
@@ -38,8 +74,32 @@ export default {
   methods: {
     getPeople() {
       axios.get("/api/people").then((res) => {
-        this.people = res.data
+        this.people = res.data;
       });
+    },
+
+    updatePerson(id) {
+      this.edidPersonId = null;
+      axios
+        .patch(`/api/people/${id}`, {
+          name: this.name,
+          age: this.age,
+          job: this.job,
+        })
+        .then((res) => {
+          this.getPeople();
+        });
+    },
+
+    changeEditPersonId(id, name, age, job) {
+      this.edidPersonId = id;
+      this.name = name;
+      this.age = age;
+      this.job = job;
+    },
+
+    isEdit(id) {
+      return this.edidPersonId === id;
     },
   },
 };
