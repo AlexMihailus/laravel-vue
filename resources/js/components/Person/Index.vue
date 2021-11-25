af64ce3789c1bb9f5829b523b24d102776aab32b
Alex Mihailus <mihailus.A.G@gmail.com>
2021-11-25 11:34:29 +0200

    add getting a list of persons

--- resources/js/components/Person/Index.vue
+++ resources/js/components/Person/Index.vue
@@ -1,12 +1,45 @@
 <template>
   <div>
-    Index
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Age</th>
+          <th scope="col">Job</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr :key="person.id" v-for="person in people">
+          <td>{{ person.name }}</td>
+          <td>{{ person.age }}</td>
+          <td>{{ person.job }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
   name: "Index",
+
+  data() {
+    return {
+      people: null,
+    };
+  },
+
+  mounted() {
+    this.getPeople();
+  },
+
+  methods: {
+    getPeople() {
+      axios.get("/api/people").then((res) => {
+        this.people = res.data;
+      });
+    },
+  },
 };
 </script>
 
